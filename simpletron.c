7200125393f2a94217d4f3566990e92d0c29d1db
Daniel Servejeira <140193667+DanielServejeira@users.noreply.github.com>
2024-12-02 23:45:38 -0300

    Update simpletron.c

--- simpletron.c
+++ simpletron.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<locale.h>
+#include <stdio.h>
+#include <locale.h>
 
 #define READ 10
 #define WRITE 11
@@ -15,42 +15,42 @@
 #define HALT 43
 #define LIMITE 100
 
-void boasVindas() {
+void boas_vindas() {
     //Boas vindas;
     printf ("\t /-=-=-=-=-=-=-=-=-=-=-=-=-=-\n"
             "\t | Bem-vindo ao Simpletron! | \n"
             "\t =-=-=-=-=-=-=-=-=-=-=-=-=-=/ \n");
 
-    //InstruÁıes
-    printf ("\nINSTRU«’ES\n"
-            "1 - Insira uma instruÁ„o ou palavra de dados por vez\n"
-            "2 - Quando forem inseridos o n˙mero da posiÁ„o e um ponto de interrogaÁ„o (?)\n"
-            "Insira, ent„o, a palavra para aquela posiÁ„o\n"
-            "3 - Insira o valor sentinela -99999 para encerrar a digitaÁ„o do programa\n");
+    //Instru√ß√µes
+    printf ("\nINSTRU√á√ïES\n"
+            "1 - Insira uma instru√ß√£o ou palavra de dados por vez\n"
+            "2 - Quando forem inseridos o n√∫mero da posi√ß√£o e um ponto de interroga√ß√£o (?)\n"
+            "Insira, ent√£o, a palavra para aquela posi√ß√£o\n"
+            "3 - Insira o valor sentinela -99999 para encerrar a digita√ß√£o do programa\n");
     printf("\n");
     system("pause");
     system("cls");
 }
 
-void limparMemoria(int *memoria[]) {
+void limpar_memoria(int *memoria[]) {
     for(int i=0; i<LIMITE; i++) {
         memoria[i] = 0;
     }
 
-    printf("\n\tA memÛria est· limpa e pronta para ser utilizada\n\n");
+    printf("\n\tA mem√≥ria est√° limpa e pronta para ser utilizada\n\n");
     system("pause");
     system("cls");
 }
 
-void despejo(int *memoria[], int acumulador, int contadorInstrucao, int registroInstrucao, int codigoOperacao, int operando){
+void despejo(int *memoria[], int acumulador, int contador_instrucao, int registro_instrucao, int codigo_operacao, int operando){
     printf ("\nREGISTROS:"
             "\nacumulador %d"
             "\ncontadorInstrucao %d"
             "\nregistroInstrucao %d"
             "\ncodigoOperacao %d"
-            "\noperando %.2d\n",acumulador,contadorInstrucao,registroInstrucao,codigoOperacao,operando);
+            "\noperando %.2d\n",acumulador,contador_instrucao,registro_instrucao,codigo_operacao,operando);
 
-    printf("\nMEM”RIA:");
+    printf("\nMEM√ìRIA:");
     printf("\n\t    0\t    1\t    2\t    3\t    4\t    5\t    6\t    7\t    8\t    9");
 
     for(int i=0; i<10; i++){
@@ -63,125 +63,125 @@ void despejo(int *memoria[], int acumulador, int contadorInstrucao, int registro
     printf("\n\n");
 }
 
-void montarSimpletron(int *memoria[]) {
-    int contadorInstrucao=00, registroInstrucao=+0000, codigoOperacao=00;
+void montar_simpletron(int *memoria[]) {
+    int contador_instrucao=00, registro_instrucao=+0000, codigo_operacao=00;
 
     do{
-        printf("%.2d ? ", contadorInstrucao);
-        scanf("%d",&registroInstrucao);
+        printf("%.2d ? ", contador_instrucao);
+        scanf("%d",&registro_instrucao);
 
-        if(registroInstrucao == -99999) break;
+        if(registro_instrucao == -99999) break;
 
-        if(registroInstrucao>=0 && registroInstrucao <=9999) {
-            memoria[contadorInstrucao] = registroInstrucao;
-            codigoOperacao = registroInstrucao/100;
+        if(registro_instrucao>=0 && registro_instrucao <=9999) {
+            memoria[contador_instrucao] = registro_instrucao;
+            codigo_operacao = registro_instrucao/100;
 
-            contadorInstrucao++;
+            contador_instrucao++;
         }
-        else printf("\n\tErro: instruÁ„o fora da faixa (entre 0 e 9999)\n\n");
-    }while(contadorInstrucao<100);
+        else printf("\n\tErro: instru√ß√£o fora da faixa (entre 0 e 9999)\n\n");
+    }while(contador_instrucao<100);
 
-    if(contadorInstrucao == 100) printf("\n\n\tMemÛria cheia\n");
+    if(contador_instrucao == 100) printf("\n\n\tMem√≥ria cheia\n");
 }
 
-void executarSimpletron(int *memoria[]) {
-    int aux, acumulador=+0000, contadorInstrucao=00, registroInstrucao=+0000, codigoOperacao=00, operando=00;
+void executar_simpletron(int *memoria[]) {
+    int aux, acumulador=+0000, contador_instrucao=00, registro_instrucao=+0000, codigo_operacao=00, operando=00;
 
-    printf("\n\tInÌcio do programa\n\n");
+    printf("\n\tIn√≠cio do programa\n\n");
 
-    for(contadorInstrucao=0; codigoOperacao != HALT && contadorInstrucao < LIMITE; contadorInstrucao++) {
-        registroInstrucao = memoria[contadorInstrucao];
-        codigoOperacao = registroInstrucao/100;
-        operando = registroInstrucao%100;
+    for(contador_instrucao=0; codigo_operacao != HALT && contador_instrucao < LIMITE; contador_instrucao++) {
+        registro_instrucao = memoria[contador_instrucao];
+        codigo_operacao = registro_instrucao/100;
+        operando = registro_instrucao%100;
 
-        switch(codigoOperacao) {
-            case READ: //LÍ uma palavra do terminal e a coloca em um local especÌfico da memÛria
-                scanf("%d",&registroInstrucao);
+        switch(codigo_operacao) {
+            case READ: //L√™ uma palavra do terminal e a coloca em um local espec√≠fico da mem√≥ria
+                scanf("%d",&registro_instrucao);
 
-                while(registroInstrucao<-9999 || registroInstrucao>9999) {
-                    printf("Insira um n˙mero v·lido (entre -9999 e 9999)\n");
-                    scanf("%d",&registroInstrucao);
+                while(registro_instrucao<-9999 || registro_instrucao>9999) {
+                    printf("Insira um n√∫mero v√°lido (entre -9999 e 9999)\n");
+                    scanf("%d",&registro_instrucao);
                 }
 
-                memoria[operando] = registroInstrucao;
+                memoria[operando] = registro_instrucao;
                 break;
-            case WRITE: //Escreve no terminal uma palavra de um local especÌfico da memÛria.
+            case WRITE: //Escreve no terminal uma palavra de um local espec√≠fico da mem√≥ria.
                 printf("%d\n", memoria[operando]);
                 break;
-            case LOAD: //Carrega no acumulador uma palavra de um local especÌfico da memÛria.
+            case LOAD: //Carrega no acumulador uma palavra de um local espec√≠fico da mem√≥ria.
                 acumulador = memoria[operando];
                 break;
-            case STORE: //Armazena em um local especÌfico da memÛria uma palavra do acumulador.
+            case STORE: //Armazena em um local espec√≠fico da mem√≥ria uma palavra do acumulador.
                 memoria[operando] = acumulador;
                 break;
-            case ADD: //Adiciona uma palavra de um local especÌfico da memÛria ‡ palavra no acumulador (o resultado fica no acumulador).
-                registroInstrucao = memoria[operando];
+            case ADD: //Adiciona uma palavra de um local espec√≠fico da mem√≥ria √† palavra no acumulador (o resultado fica no acumulador).
+                registro_instrucao = memoria[operando];
 
-                aux = registroInstrucao + acumulador;
+                aux = registro_instrucao + acumulador;
 
-                if(aux<-9999 || aux>9999) printf("\n\tErro: resultado inv·lido no acumulador\n");
+                if(aux<-9999 || aux>9999) printf("\n\tErro: resultado inv√°lido no acumulador\n");
                 else acumulador = aux;
 
                 break;
-            case SUBTRACT: //Subtrai da palavra no acumulador, uma palavra em um local especÌfico da memÛria (o resultado fica no acumulador).
-                registroInstrucao = memoria[operando];
+            case SUBTRACT: //Subtrai da palavra no acumulador, uma palavra em um local espec√≠fico da mem√≥ria (o resultado fica no acumulador).
+                registro_instrucao = memoria[operando];
 
-                aux = registroInstrucao - acumulador;
+                aux = registro_instrucao - acumulador;
 
-                if(aux<-9999 || aux>9999) printf("\n\tErro: resultado inv·lido no acumulador\n");
+                if(aux<-9999 || aux>9999) printf("\n\tErro: resultado inv√°lido no acumulador\n");
                 else acumulador = aux;
 
                 break;
-            case DIVIDE: //Divide uma palavra em um local especÌfico da memÛria pela palavra no acumulador (o resultado fica no acumulador).
-                registroInstrucao = memoria[operando];
+            case DIVIDE: //Divide uma palavra em um local espec√≠fico da mem√≥ria pela palavra no acumulador (o resultado fica no acumulador).
+                registro_instrucao = memoria[operando];
 
-                if(acumulador!=0) aux = registroInstrucao/acumulador;
-                else printf("\n\tErro: divis„o por zero\n");
+                if(acumulador!=0) aux = registro_instrucao/acumulador;
+                else printf("\n\tErro: divis√£o por zero\n");
 
-                if(aux<-9999 || aux>9999) printf("\n\tErro: resultado inv·lido no acumulador\n");
+                if(aux<-9999 || aux>9999) printf("\n\tErro: resultado inv√°lido no acumulador\n");
                 else acumulador = aux;
 
                 break;
-            case MULTIPLY: //Multiplica uma palavra em um local especÌfico da memÛria pela palavra no acumulador (o resultado fica no acumulador).
-                registroInstrucao = memoria[operando];
+            case MULTIPLY: //Multiplica uma palavra em um local espec√≠fico da mem√≥ria pela palavra no acumulador (o resultado fica no acumulador).
+                registro_instrucao = memoria[operando];
 
-                aux = registroInstrucao * acumulador;
+                aux = registro_instrucao * acumulador;
 
-                if(aux<-9999 || aux>9999) printf("\n\tErro: resultado inv·lido no acumulador\n");
+                if(aux<-9999 || aux>9999) printf("\n\tErro: resultado inv√°lido no acumulador\n");
                 else acumulador = aux;
 
                 break;
-            case BRANCH: //Desvia para um local especÌfico da memÛria
-                contadorInstrucao = operando;
+            case BRANCH: //Desvia para um local espec√≠fico da mem√≥ria
+                contador_instrucao = operando;
                 break;
-            case BRANCHNEG: //Desvia para um local especÌfico da memÛria se o acumulador for negativo.
-                if(acumulador < 0) contadorInstrucao = operando;
+            case BRANCHNEG: //Desvia para um local espec√≠fico da mem√≥ria se o acumulador for negativo.
+                if(acumulador < 0) contador_instrucao = operando;
                 break;
-            case BRANCHZERO: //Desvia para um local especÌfico da memÛria se o acumulador for zero
-                if(acumulador == 0) contadorInstrucao = operando;
+            case BRANCHZERO: //Desvia para um local espec√≠fico da mem√≥ria se o acumulador for zero
+                if(acumulador == 0) contador_instrucao = operando;
                 break;
-            case HALT: //TÈrmino, i.e., o programa completou sua tarefa
-                printf("\n\tTÈrmino do programa\n\n");
+            case HALT: //T√©rmino, i.e., o programa completou sua tarefa
+                printf("\n\tT√©rmino do programa\n\n");
                 break;
             default:
                 break;
         }
     }
 
-    if(contadorInstrucao == LIMITE) printf("\n\tTÈrmino do programa\n\n");
+    if(contador_instrucao == LIMITE) printf("\n\tT√©rmino do programa\n\n");
 
-    despejo(memoria, acumulador, contadorInstrucao, registroInstrucao, codigoOperacao, operando);
+    despejo(memoria, acumulador, contador_instrucao, registro_instrucao, codigo_operacao, operando);
 
     system("pause");
     system("cls");
 }
 
 void simpletron(int *memoria[]) {
-    boasVindas();
-    limparMemoria(memoria);
+    boas_vindas();
+    limpar_memoria(memoria);
 
-    montarSimpletron(memoria);
-    executarSimpletron(memoria);
+    montar_simpletron(memoria);
+    executar_simpletron(memoria);
 }
 
 int main() {
